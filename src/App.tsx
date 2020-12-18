81a093755475594e26097228ccb9d54cd1d4dec2
Thomas Ribeiro <thomashenrique2@gmail.com>
2020-12-17 22:02:03 -0300

    feat: rotas criadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
-function App() {
-  return (
+const App: React.FC = () => (
+  <BrowserRouter>
     <Routes />
-  );
-}
+  </BrowserRouter>
+
+);
 
 export default App;
